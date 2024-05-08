3b033146cabd8df40886850b02d4c8297e173b6e
Kishan S Patel <bishan.batel@protonmail.com>
2024-05-08 17:03:39 -0500

    added compiler features

--- src/token/Decimal.cpp
+++ src/token/Decimal.cpp
@@ -8,7 +8,6 @@
 namespace goos::token {
   Decimal::Decimal(const f64 num) : number{num} {}
 
-
   auto Decimal::get_number() const -> f64 { return number; }
 
   auto Decimal::clone() const -> Box<Token> { return crab::make_box<Decimal>(number); }
