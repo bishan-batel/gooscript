3d69584b16fc7e1c9ce7d63873a054b445f785cf
Kishan S Patel <bishan.batel@protonmail.com>
2024-05-16 01:25:06 -0500

    String Utilities for Padding

--- src/utils/str.cpp
+++ src/utils/str.cpp
@@ -3,11 +3,45 @@
 //
 
 #include "str.hpp"
-
 #include <codecvt>
 #include <locale>
+#include <sstream>
+#include <range.hpp>
 
 namespace goos::str {
+  auto repeat(const widechar character, const usize amount) -> WideString {
+    WideStringStream stream{};
+
+    for ([[maybe_unused]] const auto _: crab::range(amount)) {
+      stream << character;
+    }
+
+    return stream.str();
+  }
+
+  auto left_pad(const WideStringView string, const WideStringView padding) -> WideString {
+    WideStringStream stream{};
+
+    // const WideString padding{repeat(pad_character, amount)};
+
+    const auto add_padding{[&] { stream << padding; }};
+
+    add_padding();
+
+    for (const widechar c: string) {
+      stream << c;
+      if (c == '\n') {
+        add_padding();
+      }
+    }
+
+    return stream.str();
+  }
+
+  auto left_pad(const WideStringView string, const widechar pad_character, const usize count) -> WideString {
+    return left_pad(string, repeat(pad_character, count));
+  }
+
   auto convert(const WideStringView string) -> String {
     return std::wstring_convert<std::codecvt_utf8<widechar>>().to_bytes(string.data());
   }
--- src/utils/str.hpp
+++ src/utils/str.hpp
@@ -7,7 +7,35 @@
 #include "preamble.hpp"
 
 namespace goos::str {
-  [[nodiscard]] auto convert(WideStringView string) -> String;
+ /**
+  * @brief Creates a string with the given character repeated 'amount' times
+  * @param character The character to repeat
+  * @param amount The number of times the character should be repeated
+  */
+ [[nodiscard]] auto repeat(widechar character, usize amount) -> WideString;
 
+ /**
+  * Left pads each line of the given string with the given string
+  * @param string String to pad
+  * @param padding String to be added at the beginning of each line
+  */
+ [[nodiscard]] auto left_pad(WideStringView string, WideStringView padding) -> WideString;
+
+ /**
+  * Left pads each line of the given string with the given character 'count' count of times
+  * @param string
+  * @param pad_character The character that is repeated at the beginning of each line
+  * @param count Amount of pad_character at the beginning of each line
+  */
+ [[nodiscard]] auto left_pad(WideStringView string, widechar pad_character, usize count) -> WideString;
+
+ /**
+  * @brief Converts a wide string to a string
+  */
+ [[nodiscard]] auto convert(WideStringView string) -> String;
+
+  /**
+   * @brief Converts a string to a wide string
+   */
   [[nodiscard]] auto convert(StringView string) -> WideString;
 }
