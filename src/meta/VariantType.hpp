8c73ef35cec2f7c9dc30dad7efc626fcb986837a
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-15 20:36:03 -0500

    Created conversion operator for VariantType -> String for debug purposes

--- src/meta/VariantType.hpp
+++ src/meta/VariantType.hpp
@@ -16,4 +16,20 @@ namespace goos::meta {
     NIL,
     UNIT,
   };
+
+  inline auto operator <<(std::ostream &os, const VariantType ty) -> std::ostream& {
+    switch (ty) {
+      case VariantType::INTEGER: os << "integer";
+      case VariantType::DECIMAL: os << "decimal";
+      case VariantType::STRING: os << "string";
+      case VariantType::BOOLEAN: os << "boolean";
+      case VariantType::OBJECT: os << "object";
+      case VariantType::ARRAY: os << "array";
+      case VariantType::FUNCTION: os << "function";
+      case VariantType::NIL: os << "nil";
+      case VariantType::UNIT: os << "unit";
+    }
+
+    return os;
+  }
 }
