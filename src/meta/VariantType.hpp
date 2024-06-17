25a2bd7640d2459a5b8f496189c279db159570a1
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-16 22:19:36 -0500

    Variant to string function

--- src/meta/VariantType.hpp
+++ src/meta/VariantType.hpp
@@ -3,6 +3,7 @@
 //
 
 #pragma once
+#include "utils/hash.hpp"
 
 namespace goos::meta {
   enum class VariantType {
@@ -15,21 +16,66 @@ namespace goos::meta {
     FUNCTION,
     NIL,
     UNIT,
+    REFERENCE,
+    ANY
   };
 
   inline auto operator <<(std::ostream &os, const VariantType ty) -> std::ostream& {
     switch (ty) {
-      case VariantType::INTEGER: os << "integer";
-      case VariantType::DECIMAL: os << "decimal";
-      case VariantType::STRING: os << "string";
-      case VariantType::BOOLEAN: os << "boolean";
-      case VariantType::OBJECT: os << "object";
-      case VariantType::ARRAY: os << "array";
-      case VariantType::FUNCTION: os << "function";
-      case VariantType::NIL: os << "nil";
-      case VariantType::UNIT: os << "unit";
+      case VariantType::INTEGER: {
+        os << "integer";
+        break;
+      }
+      case VariantType::DECIMAL: {
+        os << "decimal";
+        break;
+      }
+      case VariantType::STRING: {
+        os << "string";
+        break;
+      }
+      case VariantType::BOOLEAN: {
+        os << "boolean";
+        break;
+      }
+      case VariantType::OBJECT: {
+        os << "object";
+        break;
+      }
+      case VariantType::ARRAY: {
+        os << "array";
+        break;
+      }
+      case VariantType::FUNCTION: {
+        os << "function";
+        break;
+      }
+      case VariantType::NIL: {
+        os << "nil";
+        break;
+      }
+      case VariantType::UNIT: {
+        os << "unit";
+        break;
+      }
+      case VariantType::REFERENCE: {
+        os << "reference";
+        break;
+      }
+      case VariantType::ANY: {
+        os << "any";
+        break;
+      }
     }
 
     return os;
   }
 }
+
+template<>
+struct std::hash<goos::meta::VariantType> {
+  constexpr auto operator()(goos::meta::VariantType ty) const noexcept -> usize {
+    using namespace goos;
+    return utils::hash(static_cast<std::underlying_type_t<meta::VariantType>>(ty));
+  }
+};
