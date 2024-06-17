f06c0b155ec4e19d9def1627546aec69571bf3cc
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-16 22:21:39 -0500

    Made operator functions constexpr

--- src/lexer/Operator.cpp
+++ src/lexer/Operator.cpp
@@ -3,29 +3,4 @@
 #include <stdexcept>
 
 namespace goos::lexer {
-  [[maybe_unused]]
-  auto is_assign(Operator op) -> bool {
-    return static_cast<u32>(op) & ASSIGN_FLAG;
-  }
-
-  [[maybe_unused]]
-  auto is_binary(Operator op) -> bool {
-    return static_cast<u32>(op) & BINARY_FLAG;
-  }
-
-  auto is_unary(Operator op) -> bool {
-    return static_cast<u32>(op) & UNARY_FLAG;
-  }
-
-  [[maybe_unused]]
-  auto is_delimiter(Operator op) -> bool {
-    return static_cast<u32>(op) & DELIMITER_FLAG;
-  }
-
-  [[maybe_unused]]
-  auto is_parenthetical(Operator op) -> bool {
-    const u32 bits = static_cast<u32>(op);
-    return bits & PARENTHETICAL_CLOSE_FLAG or
-           bits & PARENTHETICAL_OPEN_FLAG;
-  }
 }
--- src/lexer/Operator.hpp
+++ src/lexer/Operator.hpp
@@ -119,20 +119,27 @@ namespace goos::lexer {
     }()
   };
 
-  [[maybe_unused]]
-  auto is_binary(Operator op) -> bool;
+  [[maybe_unused]] constexpr auto is_assign(Operator op) -> bool {
+    return static_cast<u32>(op) & ASSIGN_FLAG;
+  }
 
-  [[maybe_unused]]
-  auto is_unary(Operator op) -> bool;
+  [[maybe_unused]] constexpr auto is_binary(Operator op) -> bool {
+    return static_cast<u32>(op) & BINARY_FLAG;
+  }
 
-  [[maybe_unused]]
-  auto is_assign(Operator op) -> bool;
+  [[nodiscard]] constexpr auto is_unary(Operator op) -> bool {
+    return static_cast<u32>(op) & UNARY_FLAG;
+  }
 
-  [[maybe_unused]]
-  auto is_delimiter(Operator op) -> bool;
+  [[maybe_unused]] constexpr auto is_delimiter(Operator op) -> bool {
+    return static_cast<u32>(op) & DELIMITER_FLAG;
+  }
 
-  [[maybe_unused]]
-  auto is_parenthetical(Operator op) -> bool;
+  [[maybe_unused]] constexpr auto is_parenthetical(Operator op) -> bool {
+    const u32 bits = static_cast<u32>(op);
+    return bits & PARENTHETICAL_CLOSE_FLAG or
+           bits & PARENTHETICAL_OPEN_FLAG;
+  }
 
   #undef binary
   #undef assign
