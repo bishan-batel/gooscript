0d8e51e3717f02b775c9d5176972e786b25f0ee1
Kishan S Patel <bishan.batel@protonmail.com>
2024-06-21 12:54:53 -0500

    Made operations / template programming more in line with TypeConversion.hpp

--- src/runtime/PrimitiveOperations.hpp
+++ src/runtime/PrimitiveOperations.hpp
@@ -56,36 +56,64 @@ namespace goos::runtime::primitive_operators {
   using UnaryFunction = std::function<Any(Any val)>;
 
   namespace meta {
-    template<lexer::Operator op, typename Lhs, typename Rhs=Lhs, typename WrapperOutput=Lhs, typename F>
-    auto binary(
-      F function
+    template<
+      lexer::Operator op,
+      type::convertible_primitive Lhs,
+      type::convertible_primitive Rhs,
+      type::convertible_primitive Output
+    >
+    auto binary_func(
+      std::function<Output(Lhs, Rhs)> function
     ) -> std::pair<BinaryOperands, BinaryFunction> {
-      static_assert(is_binary(op));
       return {
-        BinaryOperands{Lhs{}.get_type(), Rhs{}.get_type(), op},
-        [function](Any lhs_any, Any rhs_any) {
-          RcMut<Lhs> lhs = crab::unwrap(lhs_any.downcast<Lhs>());
-          RcMut<Rhs> rhs = crab::unwrap(rhs_any.downcast<Rhs>());
-          return crab::make_rc_mut<WrapperOutput>(function(lhs->get(), rhs->get()));
-        }
-      };
-    }
-
-    template<lexer::Operator op, type::specialized_value Input, type::specialized_value WrapperOutput, typename F>
-    auto unary(
-      F function
-    ) -> std::pair<UnaryOperands, UnaryFunction> {
-      static_assert(is_unary(op));
-      return {
-        UnaryOperands{Input{}.get_type(), op},
-        [function](Any value) {
-          return crab::make_rc_mut<WrapperOutput>(
+        BinaryOperands{type::variant_type_of_primitive<Lhs>, type::variant_type_of_primitive<Rhs>, op},
+        [function](const Any &lhs, const Any &rhs) -> Any {
+          return type::to_goos_any(
             function(
-              crab::unwrap(value.downcast<Input>())->get()
+              type::try_from_goose<Lhs>(lhs).take_unchecked(),
+              type::try_from_goose<Rhs>(rhs).take_unchecked()
             )
           );
         }
       };
+    }
+
+    template<
+      lexer::Operator op,
+      typename F
+    >
+    auto binary(const F function) -> std::pair<BinaryOperands, BinaryFunction> {
+      return binary_func<op>(std::function{function});
+    }
+
+    template<
+      lexer::Operator op,
+      type::convertible_primitive Input,
+      type::convertible_primitive Output
+    >
+    auto unary_func(
+      std::function<Output(Input)> function
+    ) -> std::pair<UnaryOperands, UnaryFunction> {
+      static_assert(is_unary(op));
+
+      return {
+        UnaryOperands{type::variant_type_of_primitive<Input>, op},
+        [function](const Any &input) -> Any {
+          return type::to_goos_any(
+            function(
+              type::try_from_goose<Input>(input).take_unchecked()
+            )
+          );
+        }
+      };
+    }
+
+    template<
+      lexer::Operator op,
+      typename F
+    >
+    auto unary(const F function) -> std::pair<UnaryOperands, UnaryFunction> {
+      return meta::unary_func<op>(std::function{function});
     }
   }
 
@@ -94,81 +122,88 @@ namespace goos::runtime::primitive_operators {
     using namespace goos::meta;
     using lexer::Operator;
 
-    #define operation(op, Lhs, Rhs, Output, lambda) binary<Operator::op, Lhs, Rhs, Output>([](const auto&lhs, const auto&rhs) {  return lambda; } )
+    #define operation(op, Lhs, Rhs, lambda) binary<Operator::op>([](const Lhs lhs, const Rhs rhs) { return lambda; })
 
     Dictionary<BinaryOperands, BinaryFunction> map{
       // Bolean Operations
-      operation(AND, Boolean, Boolean, Boolean, lhs and rhs),
-      operation(NOT_EQUALS, Boolean, Boolean, Boolean, lhs != rhs),
-      operation(OR, Boolean, Boolean, Boolean, lhs or rhs),
+      // operation(AND, Boolean, Boolean, Boolean, lhs and rhs),
+      operation(AND, bool, bool, lhs == rhs),
+      operation(NOT_EQUALS, bool, bool, lhs != rhs),
+      operation(OR, bool, bool, lhs or rhs),
 
       // String Concatanation
-      operation(ADD, GString, GString, GString, *lhs + *rhs),
-      operation(ADD, GString, Integer, GString, *lhs + std::to_wstring(rhs)),
-      operation(ADD, GString, Decimal, GString, *lhs + std::to_wstring(rhs)),
-      operation(EQUALS, GString, GString, Boolean, *lhs == *rhs),
+      operation(ADD, WideString&, WideString&, lhs + rhs),
+      operation(ADD, WideString&, i64, lhs + std::to_wstring(rhs)),
+      operation(ADD, WideString&, f64, lhs + std::to_wstring(rhs)),
+      operation(EQUALS, WideString&, WideString&, lhs == rhs),
 
       // TODO remove this for better method solution, this shit cringe af
-      #define ARRAY_APPEND(ty) std::make_pair<BinaryOperands, BinaryFunction>(\
-        BinaryOperands{VariantType::ARRAY, VariantType::ty, Operator::ADD_ASSIGN},\
-        [](Any lhs, Any rhs) {\
-          RcMut<Array> lhs_arr = crab::unwrap(lhs.downcast<Array>());\
-          lhs_arr->push(std::move(rhs));\
-          return lhs;\
-        }\
-      )
-
-      ARRAY_APPEND(BOOLEAN),
-      ARRAY_APPEND(ARRAY),
-      ARRAY_APPEND(INTEGER),
-      ARRAY_APPEND(DECIMAL),
-      ARRAY_APPEND(STRING),
-      ARRAY_APPEND(BOOLEAN),
-      ARRAY_APPEND(OBJECT),
-      ARRAY_APPEND(ARRAY),
-      ARRAY_APPEND(FUNCTION),
-      ARRAY_APPEND(NIL),
-      ARRAY_APPEND(UNIT),
-      ARRAY_APPEND(REFERENCE),
-      ARRAY_APPEND(ANY),
+      // #define ARRAY_APPEND(ty) std::make_pair<BinaryOperands, BinaryFunction>(\
+      //   BinaryOperands{VariantType::ARRAY, VariantType::ty, Operator::ADD_ASSIGN},\
+      //   [](Any lhs, Any rhs) {\
+      //     RcMut<Array> lhs_arr = crab::unwrap(lhs.downcast<Array>());\
+      //     lhs_arr->push(std::move(rhs));\
+      //     return lhs;\
+      //   }\
+      // )
+      //
+      // ARRAY_APPEND(BOOLEAN),
+      // ARRAY_APPEND(ARRAY),
+      // ARRAY_APPEND(INTEGER),
+      // ARRAY_APPEND(DECIMAL),
+      // ARRAY_APPEND(STRING),
+      // ARRAY_APPEND(BOOLEAN),
+      // ARRAY_APPEND(OBJECT),
+      // ARRAY_APPEND(ARRAY),
+      // ARRAY_APPEND(FUNCTION),
+      // ARRAY_APPEND(NIL),
+      // ARRAY_APPEND(UNIT),
+      // ARRAY_APPEND(REFERENCE),
+      // ARRAY_APPEND(ANY),
       #undef ARRAY_APPEND
 
-      // Base Integer Operations
-      operation(ADD, Integer, Integer, Integer, lhs + rhs),
-      operation(XOR, Integer, Integer, Integer, lhs xor rhs),
-      operation(BIT_OR, Integer, Integer, Integer, lhs bitor rhs),
-      operation(BIT_AND, Integer, Integer, Integer, lhs bitand rhs),
-      operation(EQUALS, Integer, Integer, Boolean, lhs == rhs),
-      operation(NOT_EQUALS, Integer, Integer, Boolean, lhs != rhs),
-      operation(GREATER, Integer, Integer, Boolean, lhs > rhs),
-      operation(LESS, Integer, Integer, Boolean, lhs < rhs),
-      operation(GREATER_OR_EQUALS, Integer, Integer, Boolean, lhs >= rhs),
-      operation(LESS_OR_EQUALS, Integer, Integer, Boolean, lhs <= rhs),
-      operation(SHIFT_LEFT, Integer, Integer, Integer, lhs << rhs),
-      operation(SHIFT_RIGHT, Integer, Integer, Integer, lhs >> rhs),
-      operation(ADD, Integer, Integer, Integer, lhs + rhs),
-      operation(SUB, Integer, Integer, Integer, lhs - rhs),
-      operation(MUL, Integer, Integer, Integer, lhs * rhs),
-      operation(DIV, Integer, Integer, Integer, lhs / rhs),
-      operation(MOD, Integer, Integer, Integer, lhs % rhs),
+      // Base i64 Operations
+      #define numeric_op(op_enum, op) \
+        operation(op_enum, f64, f64, lhs op rhs ),\
+        operation(op_enum, i64, i64, lhs op rhs ),\
+        operation(op_enum, i64, f64, static_cast<f64>(lhs) op rhs ),\
+        operation(op_enum, f64, i64, lhs op static_cast<f64>(rhs))
+
+      // Bit Operations
+      operation(XOR, i64, i64, lhs xor rhs),
+      operation(BIT_OR, i64, i64, lhs bitor rhs),
+      operation(BIT_AND, i64, i64, lhs bitand rhs),
+      operation(SHIFT_LEFT, i64, i64, lhs << rhs),
+      operation(SHIFT_RIGHT, i64, i64, lhs >> rhs),
+
+      operation(EQUALS, i64, i64, lhs == rhs),
+      operation(NOT_EQUALS, i64, i64, lhs != rhs),
+      numeric_op(GREATER, >),
+      numeric_op(LESS, <),
+      numeric_op(GREATER_OR_EQUALS, >=),
+      numeric_op(LESS_OR_EQUALS, <=),
+
+      operation(MOD, i64, i64, lhs % rhs),
+
+      numeric_op(ADD, +),
+      numeric_op(SUB, -),
+      numeric_op(MUL, *),
+      numeric_op(DIV, /),
+
+      numeric_op(ADD, +),
 
       // Floating Point
-      operation(ADD, Decimal, Decimal, Decimal, lhs + rhs),
-      operation(EQUALS, Decimal, Decimal, Boolean, lhs == rhs),
-      operation(NOT_EQUALS, Decimal, Decimal, Boolean, lhs != rhs),
-      operation(GREATER, Decimal, Decimal, Boolean, lhs > rhs),
-      operation(LESS, Decimal, Decimal, Boolean, lhs < rhs),
-      operation(GREATER_OR_EQUALS, Decimal, Decimal, Boolean, lhs >= rhs),
-      operation(LESS_OR_EQUALS, Decimal, Decimal, Boolean, lhs <= rhs),
-      operation(ADD, Decimal, Decimal, Decimal, lhs + rhs),
-      operation(SUB, Decimal, Decimal, Decimal, lhs - rhs),
-      operation(MUL, Decimal, Decimal, Decimal, lhs * rhs),
-      operation(DIV, Decimal, Decimal, Decimal, lhs / rhs),
-      operation(MOD, Decimal, Decimal, Decimal, std::fmod(lhs, rhs)),
+      operation(ADD, f64, f64, lhs + rhs),
+      operation(MOD, f64, f64, std::fmod(lhs, rhs)),
 
-      operation(EQUALS, Integer, Decimal, Boolean, std::abs(static_cast<f64>(lhs) - rhs) < 1E-4_f64),
-      operation(EQUALS, Decimal, Integer, Boolean, std::abs(static_cast<f64>(rhs) - lhs) < 1E-4_f64),
+      operation(EQUALS, f64, f64, std::abs(lhs - rhs) < 1E-6_f64),
+      operation(EQUALS, i64, f64, std::abs(static_cast<f64>(lhs) - rhs) < 1E-6_f64),
+      operation(EQUALS, f64, i64, std::abs(static_cast<f64>(rhs) - lhs) < 1E-6_f64),
+      operation(NOT_EQUALS, f64, f64, std::abs(lhs - rhs) > 1E-6_f64),
+      operation(NOT_EQUALS, i64, f64, std::abs(static_cast<f64>(lhs) - rhs) > 1E-6_f64),
+      operation(NOT_EQUALS, f64, i64, std::abs(static_cast<f64>(rhs) - lhs) > 1E-6_f64),
     };
+    #undef numeric_op
     #undef operation
 
     return map;
@@ -178,12 +213,12 @@ namespace goos::runtime::primitive_operators {
     using namespace meta;
     using lexer::Operator;
 
-    #define operation(op, Input, Output, lambda) unary<Operator::op, Input, Output>([](const auto& val) {  return lambda; } )
+    #define operation(op, Input, lambda) unary<Operator::op>([](const Input val) {  return lambda; } )
 
     Dictionary<UnaryOperands, UnaryFunction> map{
-      operation(NOT, Boolean, Boolean, not val),
-      operation(SUB, Integer, Integer, -val),
-      operation(SUB, Decimal, Decimal, -val),
+      operation(NOT, bool, not val),
+      operation(SUB, i64, -val),
+      operation(SUB, f64, -val),
     };
 
     #undef operation
