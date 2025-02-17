1385f781cd48f74d36de1280ff803d820e0b75b1
Kishan S Patel <bishan.batel@protonmail.com>
2025-02-17 00:23:28 -0800

    Fixed comp issues

--- src/json/Object.hpp
+++ src/json/Object.hpp
@@ -3,45 +3,62 @@
 //
 
 #pragma once
-#include <concepts>
 #include "Value.hpp"
+#include <concepts>
 
 namespace goos::json {
   class Object final : public Value {
     Dictionary<WideString, Box<Value>> values;
 
   public:
+
     explicit Object();
 
     auto put(WideString key, Box<Value> val) -> void;
 
-    // auto put(WideString key, WideString val) -> void;
+    template<std::derived_from<Value> T>
+    auto put(WideString key, Box<T> val) -> void {
+      put(key, std::move(val));
+    }
 
     template<typename T>
     auto put(WideString key, T val) -> void;
 
-    auto erase(const WideString &key) -> bool;
+    auto erase(const WideString& key) -> bool;
 
-    [[nodiscard]] auto get(const WideString &key) -> Option<RefMut<Value>>;
+    [[nodiscard]] auto get(const WideString& key) -> Option<RefMut<Value>>;
 
-    [[nodiscard]] auto get(const WideString &key) const -> Option<Ref<Value>>;
+    [[nodiscard]] auto get(const WideString& key) const -> Option<Ref<Value>>;
 
-    [[nodiscard]] auto operator[](const WideString &key) -> Value &;
+    [[nodiscard]] auto operator[](const WideString& key) -> Value&;
 
     template<std::derived_from<Value> T>
-    [[nodiscard]] auto get_as(const WideString &key) const -> Option<Ref<T>> {
-      return get(key).flat_map([&](Ref<Value> val) { return val->try_as<T>(); });
+    [[nodiscard]] auto get_as(const WideString& key) const -> Option<Ref<T>> {
+      return get(key) //
+        .flat_map([&](const Value& val) { return val.try_as<T>(); });
     }
 
     template<std::derived_from<Value> T>
-    [[nodiscard]] auto get_as(const WideString &key) -> Option<RefMut<T>> {
-      return get(key).flat_map([&](RefMut<Value> val) { return val->try_as<T>(); });
+    [[nodiscard]] auto get_as(const WideString& key) -> Option<RefMut<T>> {
+      return get(key).flat_map([&](RefMut<Value> val) {
+        return val->try_as<T>();
+      });
     }
 
-    auto write(std::wostream &os) const -> void override;
+    auto write(std::wostream& os) const -> void override;
 
     auto clone() const -> Box<Value> override;
 
-    auto pairs() const -> const Dictionary<WideString, Box<Value>> & { return values; }
+    auto pairs() const -> const Dictionary<WideString, Box<Value>>& {
+      return values;
+    }
   };
+
 } // namespace goos::json
+
+#include "json/utils.hpp"
+
+template<typename T>
+auto goos::json::Object::put(WideString key, T val) -> void {
+  return put(std::move(key), json::to_json(val));
+}
--- src/json/utils.hpp
+++ src/json/utils.hpp
@@ -6,36 +6,37 @@
 #include "json/Array.hpp"
 #include "json/Boolean.hpp"
 #include "json/Number.hpp"
-#include "json/Object.hpp"
 #include "json/Text.hpp"
 #include "utils/str.hpp"
 
 namespace goos::json {
 
+  class Object;
+
   template<std::integral T>
-  auto to_json(const T val) -> Box<Number> {
+  [[nodiscard]] auto to_json(const T val) -> Box<Number> {
     return crab::make_box<Number>(static_cast<f64>(val));
   }
 
   template<std::floating_point T>
-  auto to_json(const T val) -> Box<Number> {
+  [[nodiscard]] auto to_json(const T val) -> Box<Number> {
     return crab::make_box<Number>(static_cast<f64>(val));
   }
 
   template<std::convertible_to<WideString> T>
-  auto to_json(const T &val) -> Box<Text> {
+  [[nodiscard]] auto to_json(const T& val) -> Box<Text> {
     return crab::make_box<Text>(static_cast<WideString>(val));
   }
 
   template<std::convertible_to<String> T>
-  auto to_json(const T &val) -> Box<Text> {
+  [[nodiscard]] auto to_json(const T& val) -> Box<Text> {
     return to_json(str::convert(static_cast<String>(val)));
   }
 
-  inline auto to_json(const bool state) -> Box<Boolean> { return crab::make_box<Boolean>(state); }
+  [[nodiscard]] auto to_json(bool state) -> Box<Boolean>;
 
-  inline auto array() -> Box<Array> { return crab::make_box<Array>(); }
+  [[nodiscard]] auto array() -> Box<Array>;
 
-  inline auto object() -> Box<Object> { return crab::make_box<Object>(); }
+  [[nodiscard]] auto object() -> Box<Object>;
 
 } // namespace goos::json
